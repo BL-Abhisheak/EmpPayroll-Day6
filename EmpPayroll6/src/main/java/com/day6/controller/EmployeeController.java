d7fb390b6a127ce3a4cc18bd40c5e9299690beed
abhisheak.bhaskaran@bridgelabz.com <abhisheak.ba@tmachinesoftware.com>
2026-01-27 15:50:28 +0530

    DTO later added

--- EmpPayroll6/src/main/java/com/day6/controller/EmployeeController.java
+++ EmpPayroll6/src/main/java/com/day6/controller/EmployeeController.java
@@ -2,7 +2,6 @@ package com.day6.controller;
 
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,9 +12,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.day6.entity.Employee;
+import com.day6.dto.EmployeeDTO;
 import com.day6.service.EmployeeService;
-
 
 
 @RestController
@@ -23,30 +21,63 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @PostMapping("/add")
-    public Employee addEmployee(@RequestBody Employee employee) {
-        return employeeService.addEmployee(employee);
+    // @PostMapping("/add")
+    // public Employee addEmployee(@RequestBody Employee employee) {
+    //     return employeeService.addEmployee(employee);
+    // }
+
+
+
+    @PostMapping("/emp/add")
+    public EmployeeDTO addEmployee(@RequestBody EmployeeDTO dto) {
+        return employeeService.addEmployee(dto);
     }
+
+
+
+    // @GetMapping("/emp/{id}")
+    // public Optional<Employee> viewById(@PathVariable int id, Employee employee){
+    //     return employeeService.findyById(id);
+    // }
 
 
     @GetMapping("/emp/{id}")
-    public Optional<Employee> viewById(@PathVariable int id, Employee employee){
-        return employeeService.findyById(id);
+    public EmployeeDTO viewById(@PathVariable int id) {
+        return employeeService.findById(id);
     }
 
+    // @GetMapping("/emp/all")
+    // public List<Employee> viewAll(Employee emp){
+    //     return employeeService.viewAllEmp();
+    // }
+
     @GetMapping("/emp/all")
-    public List<Employee> viewAll(Employee emp){
+    public List<EmployeeDTO> viewAll() {
         return employeeService.viewAllEmp();
     }
 
+
+
+    // @PutMapping("/emp/{id}")
+    // public Employee updateEmp(@PathVariable int id,@RequestBody Employee empl){
+    //     return employeeService.updateEmp(id, empl);
+    // }
+
+
     @PutMapping("/emp/{id}")
-    public Employee updateEmp(@PathVariable int id,@RequestBody Employee empl){
-        return employeeService.updateEmp(id, empl);
+    public EmployeeDTO updateEmp(@PathVariable int id, @RequestBody EmployeeDTO dto) {
+        return employeeService.updateEmp(id, dto);
     }
 
 
+    // @DeleteMapping("/emp/{id}")
+    // public String deleteEmp(@PathVariable int id){
+    //     return employeeService.deleteEmp(id);
+    // }
+
+
     @DeleteMapping("/emp/{id}")
-    public String deleteEmp(@PathVariable int id){
+    public String deleteEmp(@PathVariable int id) {
         return employeeService.deleteEmp(id);
     }
 }
