1a8bed467d18afb89afa54003b01560850673f00
abhisheak.bhaskaran@bridgelabz.com <abhisheak.ba@tmachinesoftware.com>
2026-01-27 15:32:08 +0530

    Initail commit for the master branch

---
+++ EmpPayroll6/src/main/java/com/day6/EmpPayroll6Application.java
@@ -0,0 +1,13 @@
+package com.day6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpPayroll6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpPayroll6Application.class, args);
+	}
+
+}
